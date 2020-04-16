359c99bda18ec0b734cc11c3b4bce3d803113471
Dan <danielk@alleninstitute.org>
2020-04-15 17:00:43 -0700

    adding methods and test for manifest upload/append

--- segmentation_labeling_app/transfers/utils.py
+++ segmentation_labeling_app/transfers/utils.py
@@ -1,8 +1,10 @@
 import boto3
+from botocore.exceptions import ClientError
 import pathlib
 import sqlite3
 import json
 import jsonlines
+import tempfile
 
 
 def upload_file(file_name, bucket, key=None):
@@ -18,6 +20,12 @@ def upload_file(file_name, bucket, key=None):
         object key. If not passed, object key will be
         basename of the file_name
 
+    Returns
+    -------
+    s3_uri : str
+        URI to S3 object just uploaded
+
+
     """
     if key is None:
         key = pathlib.PurePath(file_name).name
@@ -25,7 +33,89 @@ def upload_file(file_name, bucket, key=None):
     client = boto3.client('s3')
     client.upload_file(file_name, bucket, key)
 
-    return 's3://' + bucket + '/' + key
+    s3_uri = 's3://' + bucket + '/' + key
+
+    return s3_uri
+
+
+def object_exists(bucket, key):
+    """whether an object exists in an S3 bucket
+
+    Parameters
+    ----------
+    bucket: str
+        name of bucket
+    key: str
+        object key. If not passed, object key will be
+        basename of the file_name
+
+    Returns
+    -------
+    exists : bool
+
+    """
+    exists = False
+    client = boto3.client('s3')
+    try:
+        client.head_object(Bucket=bucket, Key=key)
+        exists = True
+    except ClientError as exc:
+        pass
+    return exists
+
+
+class UploadManifestException(Exception):
+    pass
+
+
+def upload_manifest(file_name, bucket, key, mode=None):
+    """upload or update an S3 manifest
+
+    Parameters
+    ----------
+    file_name: path-like object
+        full path to local file
+    bucket: str
+        name of bucket
+    key: str
+        object key
+    mode : str
+        'w' (truncate & write) or 'a' (append) for an existing file
+
+    Returns
+    -------
+    s3_uri : str
+        URI to S3 object just uploaded
+    """
+
+    exists = object_exists(bucket, key)
+
+    if exists & (mode not in ['w', 'a']):
+        # specify a correct way to handle existing object
+        raise UploadManifestException(
+                f"s3://{bucket}/{key} exists"
+                "specify mode='a' or 'w'")
+
+    if (not exists) | (mode == 'w'):
+        # new file, or truncate and write
+        s3_uri = upload_file(file_name, bucket, key=key)
+
+    else:
+        # append
+        tfile = tempfile.NamedTemporaryFile()
+        boto3.client('s3').download_file(bucket, key, tfile.name)
+        with open(tfile.name, "r") as fp:
+            lines = fp.readlines()
+        tfile.close()
+        with open(file_name, "r") as fp:
+            lines.extend(fp.readlines())
+        tfile = tempfile.NamedTemporaryFile()
+        with open(tfile.name, "w") as fp:
+            fp.writelines(lines)
+        s3_uri = upload_file(tfile.name, bucket, key=key)
+        tfile.close()
+
+    return s3_uri
 
 
 def upload_manifest_contents(local_manifest, bucket, prefix):
--- tests/test_transfer_utils.py
+++ tests/test_transfer_utils.py
@@ -7,6 +7,7 @@ from moto import mock_s3
 import pathlib
 import numpy as np
 import string
+import filecmp
 
 
 @pytest.fixture(scope='module')
@@ -45,6 +46,31 @@ def db_file(tmpdir_factory):
     conn.execute("CREATE TABLE manifest_table (manifest text)")
     conn.close
     yield db
+
+
+@pytest.fixture(scope='module')
+def list_of_dicts():
+    letters = [i for i in string.ascii_lowercase]
+    jsons = []
+    for i in range(10):
+        jsons.append({})
+        for j in range(20):
+            key = "".join(np.random.choice(letters, 10))
+            value = "".join(np.random.choice(letters, 40))
+            jsons[-1][key] = value
+    yield jsons
+
+
+@pytest.mark.parametrize("expected", [True, False])
+def test_object_exists(local_file, bucket, expected):
+    if expected:
+        key = "object/exists/test/" + pathlib.PurePath(local_file).name
+        utils.upload_file(local_file, bucket, key=key)
+    else:
+        key = "object/does/not/exist"
+
+    exists = utils.object_exists(bucket, key)
+    assert exists == expected
 
 
 @pytest.mark.parametrize("key", ["abc/temp.csv", None])
@@ -106,25 +132,54 @@ def test_get_manifests_from_db(db_file):
             assert r[key] == test_dict[key]
 
 
-def test_manifest_file_from_jsons(tmpdir_factory):
-    letters = [i for i in string.ascii_lowercase]
-
-    jsons = []
-    for i in range(10):
-        jsons.append({})
-        for j in range(20):
-            key = "".join(np.random.choice(letters, 10))
-            value = "".join(np.random.choice(letters, 40))
-            jsons[-1][key] = value
-
+def test_manifest_file_from_jsons(list_of_dicts, tmpdir_factory):
     fn = tmpdir_factory.mktemp("manifest_file").join("manifest.jsonl")
-    utils.manifest_file_from_jsons(str(fn), jsons)
+    utils.manifest_file_from_jsons(str(fn), list_of_dicts)
 
     with open(str(fn), "r") as fp:
         lines = fp.readlines()
 
     for i, line in enumerate(lines):
         read_json = json.loads(line)
-        assert set(list(read_json.keys())) == set(list((jsons[i].keys())))
+        assert (
+                set(list(read_json.keys())) ==
+                set(list((list_of_dicts[i].keys()))))
         for key in read_json.keys():
-            assert read_json[key] == jsons[i][key]
+            assert read_json[key] == list_of_dicts[i][key]
+
+
+def test_upload_manifest(list_of_dicts, bucket, tmpdir_factory):
+    tdir = tmpdir_factory.mktemp("manifest_upload_test")
+    local_manifest = tdir.join("manifest.jsonl")
+    utils.manifest_file_from_jsons(str(local_manifest), list_of_dicts)
+
+    key = "datetime/manifest.jsonl"
+
+    # upload manifest
+    s3_uri = utils.upload_manifest(str(local_manifest), bucket, key)
+    # download and compare
+    downloaded_manifest = tdir.join('downloaded_manifest.jsonl')
+    boto3.client('s3').download_file(bucket, key, str(downloaded_manifest))
+    assert filecmp.cmp(local_manifest, downloaded_manifest)
+
+    with pytest.raises(utils.UploadManifestException):
+        s3_uri = utils.upload_manifest(str(local_manifest), bucket, key)
+
+    # upload manifest and overwrite
+    s3_uri = utils.upload_manifest(str(local_manifest), bucket, key, mode='w')
+    # download and compare
+    downloaded_manifest = tdir.join('downloaded_manifest.jsonl')
+    boto3.client('s3').download_file(bucket, key, str(downloaded_manifest))
+    assert filecmp.cmp(local_manifest, downloaded_manifest)
+
+    # upload manifest and append
+    s3_uri = utils.upload_manifest(str(local_manifest), bucket, key, mode='a')
+    # download and compare
+    downloaded_manifest = tdir.join('downloaded_manifest.jsonl')
+    boto3.client('s3').download_file(bucket, key, str(downloaded_manifest))
+    local_double = tdir.join("manifest_twice.jsonl")
+    with open(local_manifest, "r") as fp:
+        lines = fp.readlines()
+    with open(local_double, "w") as fp:
+        fp.writelines(lines + lines)
+    assert filecmp.cmp(local_double, downloaded_manifest)
