394378e608f8ad151bb1069eaa3bc10749a4212b
Kat Schelonka <kschelon@gmail.com>
2020-04-17 11:47:09 -0700

    Get content boundary as a separate function
    This makes it easier to view the source from the ROI object.

--- segmentation_labeling_app/transforms/array_utils.py
+++ segmentation_labeling_app/transforms/array_utils.py
@@ -5,6 +5,45 @@ import numpy as np
 from scipy.sparse import coo_matrix
 
 
+def content_boundary_2d(arr: Union[np.ndarray, coo_matrix]) -> np.ndarray:
+    """
+    Get the minimal row/column boundaries for content in a 2d array.
+
+    Parameters
+    ==========
+    arr: A 2d np.ndarray or coo_matrix. Other formats are also possible
+        (csc_matrix, etc.) as long as they have the toarray() method to
+         convert them to np.ndarray.
+
+    Returns
+    =======
+    4-tuple of row/column boundaries that define the minimal rectangle
+    around nonzero array content. Note that the maximum side boundaries
+    follow python indexing rules, so the value returned is the actual
+    max index + 1:
+        top_bound: smallest row index
+        bot_bound: largest row index + 1
+        left_bound: smallest column index
+        right_bound: largest column index + 1
+    """
+    if isinstance(arr, coo_matrix):
+        col = arr.col
+        row = arr.row
+    else:
+        if not isinstance(arr, np.ndarray):
+            arr = arr.toarray()
+        row, col = np.nonzero(arr)
+    if not row.size:
+        logging.warning("No content found. Either array is empty or all "
+                        "elements equal zero.")
+        return (0, 0, 0, 0)
+    left_bound = col.min()
+    right_bound = col.max() + 1
+    top_bound = row.min()
+    bot_bound = row.max() + 1
+    return top_bound, bot_bound, left_bound, right_bound
+
+
 def crop_2d_array(arr: Union[np.ndarray, coo_matrix]) -> np.ndarray:
     """
     Crop a 2d array to a rectangle surrounding all nonzero elements.
@@ -19,21 +58,13 @@ def crop_2d_array(arr: Union[np.ndarray, coo_matrix]) -> np.ndarray:
     ======
     ValueError if all elements are nonzero.
     """
-    if isinstance(arr, coo_matrix):
-        col = arr.col
-        row = arr.row
+    boundaries = content_boundary_2d(arr)
+    if not isinstance(arr, np.ndarray):
         arr = arr.toarray()
-    else:
-        if not isinstance(arr, np.ndarray):
-            arr = arr.toarray()
-        row, col = np.nonzero(arr)
-    if not row.size:
+    if sum(boundaries) == 0:
         raise ValueError("Cannot crop an empty array, or an array where all "
                          "elements are zero.")
-    left_bound = col.min()
-    right_bound = col.max() + 1
-    top_bound = row.min()
-    bot_bound = row.max() + 1
+    top_bound, bot_bound, left_bound, right_bound = boundaries
     return arr[top_bound:bot_bound, left_bound:right_bound]
 
 
--- tests/transforms/test_array_utils.py
+++ tests/transforms/test_array_utils.py
@@ -8,6 +8,37 @@ from segmentation_labeling_app.transforms import array_utils as au
 @pytest.mark.parametrize(
     "arr,expected",
     [
+        (   # Typical
+            coo_matrix(
+                (np.array([3, 2, 1]),
+                 (np.array([0, 1, 0]), np.array([0, 1, 2]))),
+                shape=(4, 4)),
+            (0, 2, 0, 3)
+        ),
+        (   # Full data
+            np.ones((10, 10)),
+            (0, 10, 0, 10),
+        ),
+        (   # No data
+            np.zeros((5, 5)),
+            (0, 0, 0, 0),
+        ),
+        (   # Corners
+            coo_matrix(
+                (np.array([9, 9]),
+                 (np.array([0, 6]), np.array([0, 6]))),
+                shape=(7, 7)),
+            (0, 7, 0, 7)
+        ),
+    ]
+)
+def test_content_boundary_2d(arr, expected):
+    assert expected == au.content_boundary_2d(arr)
+
+
+@pytest.mark.parametrize(
+    "arr,expected",
+    [
         (
             coo_matrix(
                 (np.array([3, 2, 1]),
