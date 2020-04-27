185073d7677b542c4181fa85f9659f8924554852
Dan <danielk@alleninstitute.org>
2020-04-27 12:28:34 -0700

    downsample takes ND array. Refactored downsample test

--- segmentation_labeling_app/transforms/transform_pipeline.py
+++ segmentation_labeling_app/transforms/transform_pipeline.py
@@ -5,11 +5,10 @@ import os
 import json
 import imageio
 import numpy as np
-import h5py
 import segmentation_labeling_app.utils.query_utils as query_utils
 from segmentation_labeling_app.rois.rois import ROI
 from segmentation_labeling_app.transforms.transformations import (
-        downsample_array, transform_to_mp4)
+        downsample_h5_video, transform_to_mp4)
 from segmentation_labeling_app.transforms.array_utils import (
         content_extents)
 
@@ -105,8 +104,8 @@ class TransformPipeline(argschema.ArgSchemaParser):
         query_string = ("SELECT * FROM segmentation_runs "
                         f"WHERE id={self.args['segmentation_run_id']}")
         seg_query = db_conn.query(query_string)[0]
-        with h5py.File(seg_query['source_video_path'], 'r') as h5f:
-            downsampled_video = downsample_array(h5f)
+        downsampled_video = downsample_h5_video(
+                Path(seg_query['source_video_path']))
         max_projection = np.max(downsampled_video, axis=0)
         avg_projection = np.mean(downsampled_video, axis=0)
 
--- segmentation_labeling_app/transforms/transformations.py
+++ segmentation_labeling_app/transforms/transformations.py
@@ -1,7 +1,48 @@
+from pathlib import Path
 from typing import Union
 import h5py
 import numpy as np
 import imageio_ffmpeg as mpg
+
+
+def downsample_h5_video(
+        video_path: Union[Path],
+        input_fps: int = 31,
+        output_fps: int = 4,
+        strategy: str = 'average',
+        random_seed: int = 0) -> np.ndarray:
+    """Opens an h5 file and downsamples dataset 'data'
+    along axis=0
+
+    Parameters
+    ----------
+        video_path: pathlib.Path
+            path to an h5 video. Should have dataset 'data'. For video,
+            assumes dimensions [time, width, height] and downsampling
+            applies to time.
+        input_fps: int
+            frames-per-second of the input array
+        output_fps: int
+            frames-per-second of the output array
+        strategy: str
+            downsampling strategy. 'random', 'maximum', 'average',
+            'first', 'last'. Note 'maximum' is not defined for
+            multi-dimensional arrays
+        random_seed: int
+            passed to numpy.random.default_rng if strategy is 'random'
+
+    Returns:
+        video_out: numpy.ndarray
+            array downsampled along axis=0
+    """
+    with h5py.File(video_path, 'r') as h5f:
+        video_out = downsample_array(
+                h5f['data'],
+                input_fps,
+                output_fps,
+                strategy,
+                random_seed)
+    return video_out
 
 
 def downsample_array(
@@ -28,8 +69,8 @@ def downsample_array(
             passed to numpy.random.default_rng if strategy is 'random'
 
     Returns:
-        new_video: A new video down-sampled to desired FPS with selected
-        strategy (time, row, col)
+        array_out: numpy.ndarray
+            array downsampled along axis=0
     """
     if output_fps > input_fps:
         raise ValueError('Output FPS cannot be greater than input FPS')
@@ -38,6 +79,7 @@ def downsample_array(
 
     npts_in = array.shape[0]
     npts_out = int(npts_in * output_fps / input_fps)
+    print(npts_in, npts_out)
     bin_list = np.array_split(np.arange(npts_in), npts_out)
 
     array_out = np.zeros((npts_out, *array.shape[1:]))
