0f8e9e2ab14423fef1264205a782222d32dbbf38
Adam Amster <adam.amster@alleninstitute.org>
2021-12-09 09:46:35 -0500

    correlation projection image is now 2d, not 3d

--- slapp/transforms/transform_pipeline.py
+++ slapp/transforms/transform_pipeline.py
@@ -332,7 +332,6 @@ class TransformPipeline(argschema.ArgSchemaParser):
         max_projection = np.max(video, axis=0)
         correlation_projection = plt.imread(self.args[
                                                 'correlation_projection_path'])
-        correlation_projection = correlation_projection[:, :, 0]
         movie_quantiles = [self.args['movie_lower_quantile'],
                            self.args['movie_upper_quantile']]
         proj_quantiles = [self.args['projection_lower_quantile'],
